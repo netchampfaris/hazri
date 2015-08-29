7bc03e608d09d9bdfaeccb07e0a12f81f5820c98
Faris Ansari <netchamp.faris@gmail.com>
2015-08-29 23:54:34 +0530

    add getData service which will get all data from firebase

--- www/scripts/services.js
+++ www/scripts/services.js
@@ -1,8 +1,19 @@
 angular.module('hazri.services', ['firebase'])
 
 
-        .factory("Auth", ["$firebaseAuth",
-         function ($firebaseAuth) {
-             var ref = new Firebase("https://hazri.firebaseio.com/");
-             return $firebaseAuth(ref);
-         }]);
+.factory("Auth", ["$firebaseAuth",
+    function ($firebaseAuth) {
+        var ref = new Firebase("https://hazri.firebaseio.com/");
+        return $firebaseAuth(ref);
+    }])
+
+.factory("getData",
+    function () {
+        var ref = new Firebase("https://hazri.firebaseio.com/Department/");
+        ref.on("value", function (snapshot) {
+            return snapshot;
+        }, function (error) {
+            return {};
+        });
+        
+});
