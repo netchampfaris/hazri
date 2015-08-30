ca2e4686b16e61d09bd70991ec12518723baa619
Faris Ansari <netchamp.faris@gmail.com>
2015-08-30 14:25:23 +0530

    add factory to retrieve any firebaseNode

--- www/scripts/services.js
+++ www/scripts/services.js
@@ -18,4 +18,28 @@ angular.module('hazri.services', ['firebase'])
                     });
             }
         }    
+})
+
+.factory("firebaseNode", function () {
+    
+    return {
+
+        getChildren: function (params) {
+            var list = [];
+            var str = "https://hazri.firebaseio.com/";
+            for (var int = 0; i < params.length; i++)
+                str += params[i] + "/";
+            var ref = new Firebase(str);
+            ref.orderbyKey().on("value", function (snapshot) {
+                snapshot.forEach(function (data) {
+                    list.push({ text: data.key() });
+                });
+            }, function (error) {
+                console.log(error);
+            });
+            return list;
+        }
+
+    }
+
 });
